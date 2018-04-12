1b19438ada0aa21363a044312b80b87f7db82b21
nlangholz <nathan.langholz@redbull.com>
2018-04-12 14:51:44 -0700

    slides in progress and code examples

---
+++ week-2/single-dependent-var-logistic-example.R
@@ -0,0 +1,104 @@
+# Data are at http://www.stat.columbia.edu/~gelman/arm/examples/nes
+
+#install.packages('foreign'); needed
+library(foreign)
+library(arm)
+
+## Read the data
+brdata <- read.dta("week-2/data/nes5200_processed_voters_realideo.dta",convert.factors=F)
+
+# Clean the data
+brdata <- brdata[is.na(brdata$black)==FALSE&is.na(brdata$female)==FALSE&is.na(brdata$educ1)==FALSE
+                 &is.na(brdata$age)==FALSE&is.na(brdata$income)==FALSE&is.na(brdata$state)==FALSE,]
+kept.cases <- 1952:2000
+matched.cases <- match(brdata$year, kept.cases)
+keep <- !is.na(matched.cases)
+data <- brdata[keep,]
+plotyear <- unique(sort(data$year))
+year.new <- match(data$year,unique(data$year))
+n.year <- length(unique(data$year))
+income.new <-data$income-3
+age.new <- (data$age-mean(data$age))/10
+y <- data$rep_pres_intent
+data <- cbind(data, year.new, income.new, age.new, y)
+nes.year <- data[,"year"]
+age.discrete <- as.numeric (cut (data[,"age"], c(0,29.5, 44.5, 64.5, 200)))
+race.adj <- ifelse (data[,"race"]>=3, 1.5, data[,"race"])
+data <- cbind (data, age.discrete, race.adj)
+
+female <- data[,"gender"] - 1
+black <- ifelse (data[,"race"]==2, 1, 0)
+rvote <- ifelse (data[,"presvote"]==1, 0, ifelse(data[,"presvote"]==2, 1, NA))
+
+region.codes <- c(3,4,4,3,4,4,1,1,5,3,3,4,4,2,2,2,2,3,3,1,1,1,2,2,3,2,4,2,4,1,1,4,1,3,2,2,3,4,1,
+                  1,3,2,3,3,4,1,3,4,1,2,4)
+
+
+yr <- '1992'
+ok <- nes.year==yr & data$presvote<3
+vote <- data$presvote[ok] - 1
+income <- data$income[ok]
+
+
+
+
+# Estimation
+fit.1 <- glm (vote ~ income, family=binomial(link="logit"))
+summary(fit.1)
+
+# Graph figure 5.1 (a)
+curve (invlogit(fit.1$coef[1] + fit.1$coef[2]*x), 1, 5, ylim=c(-.01,1.01),
+       xlim=c(-2,8), xaxt="n", xaxs="i", mgp=c(2,.5,0),
+       ylab="Pr (Republican vote)", xlab="Income", lwd=4)
+curve (invlogit(fit.1$coef[1] + fit.1$coef[2]*x), -2, 8, lwd=.5, add=T)
+axis (1, 1:5, mgp=c(2,.5,0))
+mtext ("(poor)", 1, 1.5, at=1, adj=.5)
+mtext ("(rich)", 1, 1.5, at=5, adj=.5)
+points (jitter (income, .5), jitter (vote, .08), pch=20, cex=.1)
+
+# Graph figure 5.1 (b)
+sim.1 <- sim(fit.1)
+curve (invlogit(fit.1$coef[1] + fit.1$coef[2]*x), .5, 5.5, ylim=c(-.01,1.01),
+       xlim=c(.5,5.5), xaxt="n", xaxs="i", mgp=c(2,.5,0),
+       ylab="Pr (Republican vote)", xlab="Income", lwd=1)
+for (j in 1:20){
+  curve (invlogit(sim.1$coef[j,1] + sim.1$coef[j,2]*x), col="gray", lwd=.5, add=T)
+}
+curve (invlogit(fit.1$coef[1] + fit.1$coef[2]*x), add=T)
+axis (1, 1:5, mgp=c(2,.5,0))
+mtext ("(poor)", 1, 1.5, at=1, adj=.5)
+mtext ("(rich)", 1, 1.5, at=5, adj=.5)
+points (jitter (income, .5), jitter (vote, .08), pch=20, cex=.1)
+
+
+## Evaluation at the mean
+
+display (fit.1)
+invlogit(coef(fit.1)[1] + coef(fit.1)[2]*mean(income, na.rm=T))
+
+## Fitting and displaying the model
+
+## Displaying the results of several logistic regressions
+
+# Figure 5.4
+
+income.year <- NULL
+income.coef <- NULL
+income.se <- NULL
+for (yr in seq(1952,2000,4)){
+  ok <- nes.year==yr & data$presvote<3
+  vote <- data$presvote[ok] - 1
+  income <- data$income[ok]
+  fit.1 <- glm (vote ~ income, family=binomial(link="logit"))
+  income.year <- c (income.year, yr)
+  income.coef <- c (income.coef, fit.1$coef[2])
+  income.se <- c (income.se, se.coef(fit.1)[2])
+}
+
+plot (income.year, income.coef, xlim=c(1950,2000), ylim=range(income.coef+income.se, 
+                                                              income.coef-income.se), mgp=c(2,.5,0), pch=20, ylab="Coefficient of income", xlab="Year")
+for (i in 1:length(income.year)){
+  lines (rep(income.year[i],2), income.coef[i]+income.se[i]*c(-1,1), lwd=.5)
+}
+abline (0,0,lwd=.5, lty=2)
+
---
+++ week-2/war-handroll-MLE-logistic-example.R
@@ -0,0 +1,65 @@
+
+###----------------------------------------------------------------
+### Fearon & Laitin. First, show predicted probabilities of war
+### at two different levels of instability
+###---------------------------------------------------------------
+load("fearon_laitin.RData")
+
+glm.out=glm(war~instab+lmtnest, data=fearon_laitin, family=binomial(link="logit"))
+#Could use predict, or get \hat{pi} by hand for the two cases:
+pi0=1/(1+exp(-glm.out$coef[1]-0-glm.out$coef[3]*fearon_laitin$lmtnest))
+pi1=1/(1+exp(-glm.out$coef[1]-glm.out$coef[2]-glm.out$coef[3]*fearon_laitin$lmtnest))
+
+cola=rgb(80,20,0,10,maxColorValue=255)
+colb=rgb(0,100,100,10,maxColorValue=255)
+
+plot_colors <- c(rgb(r=0.4,g=0.1,b=0.0),rgb(r=0,g=.5,b=.5) )
+
+#pdf("fearonlaitiny1y0.pdf", height=4, width=7)
+plot(fearon_laitin$lmtnest,pi0, ylim=c(0,.6),ylab="Pr(war)", xlab="mountainous terrain", col=cola, pch=16)
+points(fearon_laitin$lmtnest,pi1, col=colb, pch=16)
+
+abline(v=mean(fearon_laitin$lmtnest),lty=2)
+text(mean(fearon_laitin$lmtnest+.3), .4, "mean(lmtn)")
+
+legend("topleft", legend=c("Pr[war|instab=0,lmtn]", "Pr[war|instab=1,lmtn]"), 
+       col=plot_colors, pch=16)
+dev.off()
+
+#Get difference in mean:
+firstdiff_overall=mean(pi1)-mean(pi0)
+mean_lmtn=mean(fearon_laitin$lmtnest)
+
+#Compare to approach of first fixing the other covariates
+#at their mean and then computing 
+#tau = Pr(Y=1|T=1,X=mean(X))-Pr(Y=1|T=0,X=mean(X))
+firstdiff_fixedmean=1/(1+exp(-glm.out$coef[1]-glm.out$coef[2]-glm.out$coef[3]*mean_lmtn))-1/(1+exp(-glm.out$coef[1]-0-glm.out$coef[3]*mean_lmtn))
+
+###----------------------------------------------------------
+### Hand-rolled optimization for a logit model
+###_---------------------------------------------------------
+#Logit log-likelihood
+loglik=function(par, X, y){  
+  #check if there is already an intercept or not
+  if (prod(apply(X,2,var))!=0) X=cbind(1,X)  
+  
+  #compute likelihoods for each individual (vectorize, so all at once)
+  pi_est=1/(1+exp(-1*X%*%par))
+  #log likelihood is sum of logs of individual likelihoods
+  ll=sum(y*log(pi_est)+(1-y)*log(1-pi_est))
+  return(ll)
+}
+
+X=as.matrix(fearon_laitin[,c("instab","lmtnest")])
+y=fearon_laitin$war
+
+#use optim to maximize likelihood, BFGS procedure
+opt.out = optim(par = c(0,0,0), fn = loglik, X=X, y = y,
+                method = "BFGS", control = list(fnscale = -1),
+                hessian = TRUE)
+
+#See chosen coefficients
+opt.out$par
+
+#check 
+glm(war~instab+lmtnest, data=fearon_laitin, family=binomial(link="logit"))$coef
