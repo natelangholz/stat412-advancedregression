367f1379cf0cbf709ba9ff792797bd820cb39539
nlangholz <nathan.langholz@redbull.com>
2018-04-24 09:22:26 -0700

    update r code

--- week-4/gators-multinomial-example.R
+++ week-4/gators-multinomial-example.R
@@ -1,3 +1,7 @@
+#in the following code we chose the multinom function because it does not require the data to be reshaped (as the mlogit package does) 
+#this resides in the nnet library
+library(nnet)
+
 gators <- read.csv("week-4/gators.txt")
 
 ## Descriptive
@@ -23,13 +27,13 @@ New <- data.frame(Size="large", Lake=levels(gators$Lake))
 p <- predict(fit, New, type="prob")
 col <- c("#FF4E37FF", "#979B00FF", "#00BE5DFF", "#00AFFFFF", "#FF00FFFF")
 matplot(p, xaxt="n", pch=19, col=col, type="o", lty=1, ylab="Pr")
-axis(1, at=1:4, labels=levels(Lake))
+axis(1, at=1:4, labels=levels(gators$Lake))
 legend("topright", colnames(p), col=col, pch=19, lwd=1)
 
 New <- data.frame(Lake="Trafford", Size=levels(gators$Size))
 p <- predict(fit, New, type="prob")
 matplot(p, xaxt="n", pch=19, col=col, type="o", lty=1, ylab="Pr")
-axis(1, at=1:2, labels=levels(Size))
+axis(1, at=1:2, labels=levels(gators$Size))
 legend("topright", colnames(p), col=col, pch=19, lwd=1)
 
 ## ORs
@@ -52,3 +56,5 @@ anova(fit0, fit, test="Chisq")
 
 fit0 <- multinom(Food~Lake, trace=FALSE, data = gators)
 anova(fit0, fit, test="Chisq")
+
+
