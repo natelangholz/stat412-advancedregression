92f1dbf5d2896ec95588f1f3de2b3cd2166c1c2f
nlangholz <nathan.langholz@redbull.com>
2018-04-25 22:22:29 -0700

    hurdle example and data. still need to comment better.

---
+++ week-4/doctor-visits-hurdle-example.R
@@ -0,0 +1,50 @@
+
+library(pscl)
+
+NMES1988 <- read.csv('week-4/NMES1988.txt')
+nmes <- NMES1988[, c('visits','hospital','health','chronic','gender','school','insurance')]
+
+plot(table(nmes$visits))
+
+sum(nmes$visits < 1)
+
+sum(nmes$visits > 50)
+
+mod1 <- glm(visits ~ ., data = nmes, family = "poisson")
+
+mu <- predict(mod1, type = "response")
+exp <- sum(dpois(x = 0, lambda = mu))
+round(exp)
+sum(nmes$visits < 1)
+
+mod.hurdle <- hurdle(visits ~ ., data = nmes)
+# same as this:
+# mod.hurdle <- hurdle(visits ~ ., data = nmes, dist = "poisson", zero.dist = "binomial")
+
+summary(mod.hurdle)
+
+sum(predict(mod.hurdle, type = "prob")[,1])
+
+# First 5 expected counts
+predict(mod.hurdle, type = "response")[1:5]
+
+# ratio of non-zero probabilities
+predict(mod.hurdle, type = "zero")[1:5]
+
+# mean for untruncated process
+predict(mod.hurdle, type = "count")[1:5]
+
+# multiply ratio and mean
+predict(mod.hurdle, type = "zero")[1:5] * predict(mod.hurdle, type = "count")[1:5]
+
+# equals hurdle model expected count
+predict(mod.hurdle, type = "response")[1:5]
+
+mod.hurdle.nb <- hurdle(visits ~ ., data = nmes, dist = "negbin")
+
+AIC(mod.hurdle)
+AIC(mod.hurdle.nb) # lower is better
+
+mod.hurdle.nb2 <- hurdle(visits ~ . | gender + insurance, data = nmes, dist = "negbin")
+
+AIC(mod.hurdle.nb2)
