0199cf9cec3c554dd628fb66131eebd27f17d06c
nlangholz <nathan.langholz@redbull.com>
2018-04-26 17:03:36 -0700

    update slides and hurdle example

--- week-4/doctor-visits-hurdle-example.R
+++ week-4/doctor-visits-hurdle-example.R
@@ -4,47 +4,69 @@ library(pscl)
 NMES1988 <- read.csv('week-4/data/NMES1988.txt')
 nmes <- NMES1988[, c('visits','hospital','health','chronic','gender','school','insurance')]
 
-plot(table(nmes$visits))
+plot(table(nmes$visits), ylab = 'Count',xlab = 'Visits')
 
 sum(nmes$visits < 1)
 
 sum(nmes$visits > 50)
 
-mod1 <- glm(visits ~ ., data = nmes, family = "poisson")
+fit1 <- glm(visits ~ ., data = nmes, family = "poisson")
+fit.nb <- glm.nb(visits ~ ., data = nmes)
 
-mu <- predict(mod1, type = "response")
+mu <- predict(fit1, type = "response")
 exp <- sum(dpois(x = 0, lambda = mu))
 round(exp)
+
+table(round(exp(mu)))
+
+theta <- 1.2066
+mu.nb <- predict(fit.nb , type = "response")
+exp <-sum(dnbinom(x = 0, size = theta,mu = mu.nb))
+round(exp)
+
+
 sum(nmes$visits < 1)
 
-mod.hurdle <- hurdle(visits ~ ., data = nmes)
+fit.hurdle <- hurdle(visits ~ ., data = nmes)
 # same as this:
-# mod.hurdle <- hurdle(visits ~ ., data = nmes, dist = "poisson", zero.dist = "binomial")
+# fit.hurdle <- hurdle(visits ~ ., data = nmes, dist = "poisson", zero.dist = "binomial")
 
-summary(mod.hurdle)
+summary(fit.hurdle)
 
-sum(predict(mod.hurdle, type = "prob")[,1])
+sum(predict(fit.hurdle, type = "prob")[,1])
 
 # First 5 expected counts
-predict(mod.hurdle, type = "response")[1:5]
+predict(fit.hurdle, type = "response")[1:5]
 
-# ratio of non-zero probabilities
-predict(mod.hurdle, type = "zero")[1:5]
+
+# ratio of non-zero probabilities (1 - type = 'prob' prediction)
+predict(fit.hurdle, type = "zero")[1:5]
 
 # mean for untruncated process
-predict(mod.hurdle, type = "count")[1:5]
+predict(fit.hurdle, type = "count")[1:5]
 
 # multiply ratio and mean
-predict(mod.hurdle, type = "zero")[1:5] * predict(mod.hurdle, type = "count")[1:5]
+predict(fit.hurdle, type = "zero")[1:5] * predict(fit.hurdle, type = "count")[1:5]
 
 # equals hurdle model expected count
-predict(mod.hurdle, type = "response")[1:5]
+predict(fit.hurdle, type = "response")[1:5]
 
-mod.hurdle.nb <- hurdle(visits ~ ., data = nmes, dist = "negbin")
+#install.packages("countreg", repos="http://R-Forge.R-project.org")
+library(countreg)
+rootogram(fit1, max = 80)
+rootogram(fit.nb, max = 80)
 
-AIC(mod.hurdle)
-AIC(mod.hurdle.nb) # lower is better
+rootogram(fit.hurdle, max = 80) # fit up to count 80
 
-mod.hurdle.nb2 <- hurdle(visits ~ . | gender + insurance, data = nmes, dist = "negbin")
+fit.hurdle.nb <- hurdle(visits ~ ., data = nmes, dist = "negbin")
 
-AIC(mod.hurdle.nb2)
+AIC(fit1)
+AIC(fit.nb)
+AIC(fit.hurdle)
+AIC(fit.hurdle.nb) # lower is better
+
+rootogram(fit.hurdle.nb, max = 80) # fit up to count 80
+
+fit.hurdle.nb2 <- hurdle(visits ~ . | gender + insurance, data = nmes, dist = "negbin")
+
+AIC(fit.hurdle.nb2)
