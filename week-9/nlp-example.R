54f0aadf6e9a83f38954016f686412dbe3c3458c
nlangholz <nathan.langholz@redbull.com>
2018-05-31 17:27:33 -0700

    add nlp example

--- week-9/nlp-example.R
+++ week-9/nlp-example.R
@@ -1,9 +1,4 @@
-https://github.com/statsmaths/useR2017_nlp
 
-knitr::opts_chunk$set(echo = TRUE)
-library(dplyr)
-options(dplyr.print_max = 6, dplyr.print_min = 6)
-options(width = 68)
 
 #packages
 library(cleanNLP)
@@ -37,7 +32,7 @@ letranger_tokens
 letranger_tokens$word
 
 
-paths <- dir("data/holmes_stories", full.names = TRUE)
+paths <- dir("week-9/data/holmes_stories", full.names = TRUE)
 sh_meta <- data_frame(id = as.integer(seq_along(paths)),
                       story = stri_sub(basename(paths), 4, -5))
 sh_meta %>% print(n = 5)
@@ -96,17 +91,14 @@ sh_tokens %>%
   count(id, word, sort = TRUE) %>%
   ungroup()
 
-## ---- echo = FALSE-------------------------------------------------------
 temp <- sh_tokens %>%
   count(id, word, sort = TRUE) %>%
   ungroup()
 arrange(temp, id)[5:10,]
 
-## ------------------------------------------------------------------------
-stopwords <- readLines("data/stopwords_en.txt")
+stopwords <- readLines("week-9/data/stopwords_en.txt")
 sample(stopwords, 25L)
 
-## ------------------------------------------------------------------------
 sh_toptokens <- sh_tokens %>%
   filter(!(tolower(word) %in% stopwords)) %>%
   count(id, word, sort = TRUE) %>%
@@ -114,16 +106,14 @@ sh_toptokens <- sh_tokens %>%
   top_n(n = 10, n) %>%
   left_join(sh_meta, by = "id")
 
-## ---- echo=FALSE---------------------------------------------------------
 sh_toptokens <- ungroup(sh_toptokens)
 
-## ------------------------------------------------------------------------
 sh_toptokens %>% filter(id == 1) %>% print(n = Inf)
 
-## ------------------------------------------------------------------------
+
 sh_toptokens %>% filter(id == 44) %>% print(n = Inf)
 
-## ------------------------------------------------------------------------
+
 sh_propn <- sh_tokens %>%
   filter(!(tolower(word) %in% stopwords)) %>%
   filter((tolower(word) != word)) %>%
@@ -132,7 +122,6 @@ sh_propn <- sh_tokens %>%
   top_n(n = 10, n) %>%
   left_join(sh_meta, by = "id")
 
-## ---- echo=FALSE---------------------------------------------------------
 sh_propn <- ungroup(sh_propn)
 
 ## ------------------------------------------------------------------------
@@ -206,54 +195,32 @@ sh_wordfreq <- sh_tokens %>%
   left_join(sh_meta, by = "id") %>%
   arrange(id, desc(n))
 
-## ---- echo = FALSE-------------------------------------------------------
 sh_wordfreq <- ungroup(sh_wordfreq)
 
 ## ------------------------------------------------------------------------
 sh_wordfreq %>% filter(id == 1) %>% print(n = 12)
 
 #############################
+#Part2
 
 
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
-library(dplyr)
-options(dplyr.print_max = 6, dplyr.print_min = 6)
-options(width = 68)
 
-## ---- message=FALSE------------------------------------------------------
-library(cleanNLP)
-library(dplyr)
-library(readr)
-library(stringi)
-library(ggplot2)
-theme_set(theme_minimal())
-
-## ---- eval = FALSE-------------------------------------------------------
 ## library(cleanNLP)
 ## init_spaCy(model_name = "en")
 ## anno <- run_annotators(paths)
 ## nlp <- get_combine(anno)
 
-## ---- eval = FALSE-------------------------------------------------------
 ## library(cleanNLP)
 ## init_coreNLP(language = "en")
 ## anno <- run_annotators(paths)
 ## nlp <- get_combine(anno)
 
-## ---- echo = FALSE, message = FALSE--------------------------------------
-nlp <- read_csv("data/sh_nlp.csv.gz")
+nlp <- read_csv("week-9/data/sh_nlp.csv.gz")
 
-## ---- echo = FALSE-------------------------------------------------------
 nlp
 
-## ---- message = FALSE----------------------------------------------------
-paths <- dir("data/holmes_stories", full.names = TRUE)
-sh_meta <- data_frame(id = seq_along(paths),
-                      story = stri_sub(basename(paths), 4, -5))
-sh_nlp <- read_csv("data/sh_nlp.csv.gz")
+sh_nlp <- read_csv("week-9/data/sh_nlp.csv.gz")
 
-## ---- eval = FALSE-------------------------------------------------------
 ## sh_nlp %>%
 ##   group_by(id, sid) %>%
 ##   mutate(sentence_end = tid == max(tid)) %>%
@@ -263,7 +230,6 @@ sh_nlp <- read_csv("data/sh_nlp.csv.gz")
 ##     geom_line() +
 ##     geom_point()
 
-## ---- echo = FALSE-------------------------------------------------------
 sh_nlp %>%
   group_by(id, sid) %>%
   mutate(sentence_end = tid == max(tid)) %>%
@@ -274,8 +240,7 @@ sh_nlp %>%
   geom_point()
 
 ## ------------------------------------------------------------------------
-sh_nlp %>% filter(tolower(word) != lemma) %>%
-  select(word, lemma) %>% print(n = 10)
+sh_nlp %>% filter(tolower(word) != lemma) %>% dplyr::select(word, lemma) %>% print(n = 10)
 
 ## ------------------------------------------------------------------------
 sh_lemmafr <- sh_nlp %>%
@@ -289,7 +254,6 @@ sh_lemmafr <- sh_nlp %>%
   left_join(sh_meta, by = "id") %>%
   arrange(id, desc(n))
 
-## ---- echo = FALSE-------------------------------------------------------
 sh_lemmafr <- ungroup(sh_lemmafr)
 
 ## ------------------------------------------------------------------------
@@ -304,7 +268,6 @@ sh_topchar <- sh_nlp %>%
   left_join(sh_meta, by = "id") %>%
   arrange(id, desc(n))
 
-## ---- echo = FALSE-------------------------------------------------------
 sh_topchar <- ungroup(sh_topchar)
 
 ## ------------------------------------------------------------------------
@@ -322,15 +285,15 @@ sh_compound <- sh_nlp %>%
   summarize(id = first(id), sid = first(sid),
             tid = first(tid),
             thing = stri_c(word, collapse = " ")) %>%
-  select(-d) %>%
+  dplyr::select(-d) %>%
   inner_join(sh_nlp, by = c("id", "sid", "tid"))
 
 ## ------------------------------------------------------------------------
-sh_compound %>% select(id, thing) %>% print(n = 10)
+sh_compound %>% dplyr::select(id, thing) %>% print(n = 10)
 
 ## ------------------------------------------------------------------------
 results <- sh_nlp %>%
-  select(id, entity, entity_type) %>%
+  dplyr::select(id, entity, entity_type) %>%
   filter(!is.na(entity))
 
 ## ------------------------------------------------------------------------
@@ -338,7 +301,7 @@ results %>% print(n = 10)
 
 ## ------------------------------------------------------------------------
 sh_nerchar <- sh_nlp %>%
-  select(id, entity, entity_type) %>%
+  dplyr::select(id, entity, entity_type) %>%
   filter(!is.na(entity)) %>%
   filter(entity_type == "PERSON") %>%
   count(id, entity) %>%
@@ -355,7 +318,7 @@ sh_nerchar %>% filter(id == 1) %>% print(n = Inf)
 
 ## ------------------------------------------------------------------------
 sh_nlp %>% filter(id == 1, sid == 1) %>%
-  select(word, source, relation, word_source)
+  dplyr::select(word, source, relation, word_source)
 
 ## ------------------------------------------------------------------------
 sh_whatchar <- sh_nlp %>%
@@ -364,7 +327,6 @@ sh_whatchar <- sh_nlp %>%
   count(id, word, lemma_source) %>%
   filter(n > 1)
 
-## ---- echo = FALSE-------------------------------------------------------
 sh_whatchar <- ungroup(sh_whatchar)
 
 ## ------------------------------------------------------------------------
@@ -376,42 +338,25 @@ sh_whatchar %>% print(n = 12)
 #Part 3
 
 
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
-library(dplyr)
-options(dplyr.print_max = 6, dplyr.print_min = 6)
-options(width = 68)
-
-## ---- message=FALSE------------------------------------------------------
-library(cleanNLP)
-library(dplyr)
-library(readr)
-library(stringi)
-library(ggplot2)
 library(topicmodels)
 library(glmnet)
 library(ggrepel)
-library(viridis)
-library(magrittr)
-theme_set(theme_minimal())
 
-## ---- message = FALSE----------------------------------------------------
-sotu_nlp <- read_csv("data/sotu.csv.gz")
-sotu_meta <- read_csv("data/sotu_meta.csv")
 
-## ------------------------------------------------------------------------
+sotu_nlp <- read_csv("week-9/data/sotu.csv.gz")
+sotu_meta <- read_csv("week-9/data/sotu_meta.csv")
+
 sotu_nlp %>%
   count(id, sid) %$%
   quantile(n, seq(0,1,0.1))
 
-## ------------------------------------------------------------------------
+
 sotu_nlp %>%
   filter(upos == "NOUN") %>%
   count(lemma) %>%
   top_n(n = 40, n) %>%
   use_series(lemma)
 
-## ---- eval = FALSE-------------------------------------------------------
 ## sotu_nlp %>%
 ##   count(id) %>%
 ##   group_by(id) %>%
@@ -421,7 +366,6 @@ sotu_nlp %>%
 ##     geom_point(aes(color = sotu_type)) +
 ##     geom_smooth()
 
-## ---- echo = FALSE, message = FALSE--------------------------------------
 sotu_nlp %>%
   count(id) %>%
   group_by(id) %>%
@@ -440,37 +384,34 @@ sotu_nlp %>%
   ylab("Number of words") +
   labs(color = "SOTU Address type")
 
-## ------------------------------------------------------------------------
 summary_2001 <- sotu_nlp %>%
   left_join(sotu_meta, by = "id") %>%
   filter(year == 2001, relation == "dobj") %>%
   left_join(word_frequency, by = "word") %>%
   filter(frequency < 0.001) %>%
-  select(id, word, word_source) %$%
+  dplyr::select(id, word, word_source) %$%
   sprintf("%s => %s", word_source, word)
 
 ## ------------------------------------------------------------------------
 summary_2001
 
-## ---- echo = FALSE-------------------------------------------------------
 summary_2002 <- sotu_nlp %>%
   left_join(sotu_meta, by = "id") %>%
   filter(year == 2002, relation == "dobj") %>%
   left_join(word_frequency, by = "word") %>%
   filter(frequency < 0.001) %>%
-  select(id, word, word_source) %$%
+  dplyr::select(id, word, word_source) %$%
   sprintf("%s => %s", word_source, word)
 
 ## ------------------------------------------------------------------------
 head(summary_2002, 34)
 
-## ---- echo = FALSE-------------------------------------------------------
 summary_1919 <- sotu_nlp %>%
   left_join(sotu_meta, by = "id") %>%
   filter(year == 1919, relation == "dobj") %>%
   left_join(word_frequency, by = "word") %>%
   filter(frequency < 0.001) %>%
-  select(id, word, word_source) %$%
+  dplyr::select(id, word, word_source) %$%
   sprintf("%s => %s", word_source, word)
 
 ## ------------------------------------------------------------------------
@@ -489,7 +430,7 @@ dim(sotu_tfidf$tfidf)
 
 ## ------------------------------------------------------------------------
 sotu_pca <- tidy_pca(sotu_tfidf$tfidf, sotu_meta)
-select(sotu_pca, president, party, PC1, PC2)
+dplyr::select(sotu_pca, president, party, PC1, PC2)
 
 ## ---- eval = FALSE-------------------------------------------------------
 ## ggplot(sotu_pca, aes(PC1, PC2)) +
@@ -510,14 +451,12 @@ ggplot(sotu_pca, aes(PC1, PC2)) +
         axis.text.x = element_blank(),
         axis.text.y = element_blank())
 
-## ---- eval = FALSE-------------------------------------------------------
 ## sotu_tf <- sotu_nlp %>%
 ##   filter(pos %in% c("NN", "NNS")) %>%
 ##   get_tfidf(min_df = 0.05, max_df = 0.95,
 ##                       type = "tf", tf_weight = "raw")
 ## tm <- LDA(sotu_tf$tf, k = 16, control = list(verbose = 1))
 
-## ---- echo = FALSE-------------------------------------------------------
 # this takes a few minutes to run and we don't want
 # to wait everytime for it to finish, so load from
 # cache... also, the results are stochastic (and seem
@@ -527,7 +466,7 @@ sotu_tf <- sotu_nlp %>%
   filter(pos %in% c("NN", "NNS")) %>%
   get_tfidf(min_df = 0.05, max_df = 0.95,
             type = "tf", tf_weight = "raw")
-tm <- read_rds("data/sotu_tm.Rds")
+tm <- read_rds("week-9/data/sotu_tm.Rds")
 
 ## ------------------------------------------------------------------------
 terms <- posterior(tm)$terms
@@ -551,7 +490,6 @@ topic_df$topic_new <- index[topic_df$topic]
 top_terms_df <- data_frame(top_terms, topic = 1:length(top_terms))
 top_terms_df$topic_new <- index[top_terms_df$topic]
 
-## ---- echo = FALSE-------------------------------------------------------
 ggplot(topic_df, aes(year, topic_new)) +
   geom_point(aes(size = val, color = factor(topic_new))) +
   geom_text(data = top_terms_df, x = mean(topic_df$year),
@@ -568,7 +506,6 @@ ggplot(topic_df, aes(year, topic_new)) +
   xlab("Year") +
   guides(colour = FALSE)
 
-## ------------------------------------------------------------------------
 df <- sotu_nlp %>%
   left_join(sotu_meta, by = "id") %>%
   filter(president %in% c("Barack Obama", "George W. Bush")) %>%
@@ -577,20 +514,18 @@ df <- sotu_nlp %>%
 mat <- get_tfidf(df, min_df = 0, max_df = 1, type = "tf",
                  tf_weight = "raw", doc_var = "new_id")
 
-## ---- message = FALSE----------------------------------------------------
 m2 <- data_frame(new_id = mat$id) %>%
   left_join(df[!duplicated(df$new_id),]) %>%
   mutate(y = as.numeric(president == "Barack Obama")) %>%
   mutate(train = (year %% 2 == 0))
 
-## ---- message = FALSE----------------------------------------------------
 model <- cv.glmnet(mat$tf[m2$train,], m2$y[m2$train],
                    family = "binomial", alpha = 0.9)
 
 ## ------------------------------------------------------------------------
 m2$pred <- predict(model, newx = mat$tf, type = "response",
                    s = model$lambda.1se)
-select(m2, new_id, id, sid, president, year, pred)
+dplyr::select(m2, new_id, id, sid, president, year, pred)
 
 ## ---- eval = FALSE-------------------------------------------------------
 ## ggplot(m2, aes(factor(year), pred)) +
@@ -642,11 +577,9 @@ ggplot(m2, aes(factor(year),pred)) +
   ylab("Predicted probability") +
   xlab("Year")
 
-## ---- message = FALSE----------------------------------------------------
 beta <- coef(model, s = model[["lambda"]][9])[-1]
 sprintf("%s (%d)", mat$vocab, sign(beta))[beta != 0]
 
-## ---- echo = FALSE, message = FALSE--------------------------------------
 df <- sotu_nlp %>%
   left_join(sotu_meta) %>%
   filter(president %in% c("William J. Clinton", "George Washington")) %>%
@@ -673,7 +606,6 @@ ggplot(m2, aes(factor(year),pred)) +
   ylab("Predicted probability") +
   xlab("Year")
 
-## ---- echo = FALSE-------------------------------------------------------
 beta <- coef(model, s = model[["lambda"]][12])[-1]
 sprintf("%s (%d)", mat$vocab, sign(beta))[beta != 0]
 
